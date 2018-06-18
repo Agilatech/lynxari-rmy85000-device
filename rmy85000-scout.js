8bb9ea5cd27cd91962f5a99b0850356cf0814303
Agilatech <scott@agilatech.com>
2018-06-18 09:51:45 -0600

    Initial commit

---
+++ rmy85000-scout.js
@@ -0,0 +1,50 @@
+const config = require('./config');
+const Scout = require(process.lynxari.scout);
+const Rmy85000 = require('./rmy85000');
+
+module.exports = class Rmy85000Scout extends Scout {
+
+  constructor(opts) {
+
+    super();
+
+    if (typeof opts !== 'undefined') {
+      // copy all config options defined in the server
+      for (const key in opts) {
+        if (typeof opts[key] !== 'undefined') {
+          config[key] = opts[key];
+        }
+      }
+    }
+
+    if (config.name === undefined) { config.name = "RMY85000" }
+    this.name = config.name;
+
+    this.rmy85000 = new Rmy85000(config);
+
+  }
+
+  init(next) {
+    const query = this.server.where({name: this.name});
+  
+    const self = this;
+
+    this.server.find(query, function(err, results) {
+      if (!err) {
+        if (results[0]) {
+          self.provision(results[0], self.rmy85000);
+          self.server.info('Provisioned known device ' + self.name);
+        } else {
+          self.discover(self.rmy85000);
+          self.server.info('Discovered new device ' + self.name);
+        }
+      }
+      else {
+        self.server.error(err);
+      }
+    });
+
+    next();
+  }
+
+}
---
+++ rmy85000.js
@@ -0,0 +1,17 @@
+const LynxariDevice = require(process.lynxari.device);
+const device = require('@agilatech/rmy85000');
+
+module.exports = class Rmy85000 extends LynxariDevice {
+    
+    constructor(config) {
+        
+        // The file must be defined. If not supplied in config, then default to ttyS0
+        const file  = config['file'] || "/dev/ttyS0";
+        
+        const hardware = new device.Rmy85000(file);
+        
+        super(hardware, config);
+
+    }
+}
+
